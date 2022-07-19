045725ff37a61db66ab8b26396ff9ede5114de03
James Hortman <jamesdhortman@gmail.com>
2022-07-19 14:09:24 -0400

    Created a lambda expression that takes an integer and returns the next odd number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,17 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
-    
+    public static OddNumberFinder nextOddNum = (x) -> x += (x % 2 == 0) ? 1 : 2;
+
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
+    }
+    @FunctionalInterface
+    interface OddNumberFinder{
+        int applyAsInt(int num);
     }
 }
